022f002749708c6b5d260ccb20acd53b96c947cb
doridorijamjam2 <hoc1018@naver.com>
2021-10-27 21:43:02 +0900

    스프링 시큐리티 로그인 페이지

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cos.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
@@ -10,3 +11,12 @@ import com.cos.blog.model.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
+
+
+
+//JPA Naming 쿼리
+//SELECT * FROM user WHRER username ?1 AND password =?2;
+//	User findByUsernameAndPassword(String username, String password);
+
+//	@Query(value="SELECT * FROM user WHRER username ?1 AND password =?2", nativeQuery = true)
+//	User login(String username, String password);
---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,22 @@
+package com.cos.blog.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+
+//스프링이 컴포넌트 스캔을 통해 Bean에 등록을 해줌. IoC를 해준다.
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional
+	public void 회원가입(User user) {
+			userRepository.save(user);
+	}
+}
